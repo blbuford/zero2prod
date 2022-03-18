8962db7cf33247b5a3cf604aba657f5d8b08bce1
Brett Buford <blbuford@gmail.com>
2022-03-18 08:58:01 -0700

    more logging, inb4 bunyan

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{HttpResponse, web};
 use sqlx::{PgPool};
 use chrono::Utc;
 use uuid::Uuid;
+use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -14,16 +15,18 @@ pub async fn subscribe(
     pool: web::Data<PgPool>
 ) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+    let request_span = tracing::info_span!(
+        "Adding as a new subscriber.",
+        %request_id,
+        subscriber_email=%form.email,
+        subscriber_name=%form.name
     );
-    log::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!(
+        "Saving new subscriber details in the database"
     );
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -34,24 +37,25 @@ pub async fn subscribe(
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
-    .await
-    {
-        Ok(_) => {
-            log::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        },
-        Err(e) => {
-            log::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
-            HttpResponse::InternalServerError().finish()
-        }
+        .execute(pool.get_ref())
+        .instrument(query_span)
+        .await
+        {
+            Ok(_) => {
+                tracing::info!(
+                    "request_id {} - New subscriber details have been saved",
+                    request_id
+                );
+                HttpResponse::Ok().finish()
+            },
+            Err(e) => {
+                tracing::error!(
+                    "request_id {} - Failed to execute query: {:?}",
+                    request_id,
+                    e
+                );
+                HttpResponse::InternalServerError().finish()
+            }
     }
 
 }
