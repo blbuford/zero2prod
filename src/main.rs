eb206fedf56e7cf3009dcd8f718322f820554ae3
Brett Buford <blbuford@gmail.com>
2022-03-21 23:42:12 -0700

    add TEST_LOG env var to testing module

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use zero2prod::{configuration::get_configuration, startup::run, telemetry::*};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber(
+        "zero2prod".into(),
+        "info".into(),
+        std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -76,8 +76,16 @@ async fn subscribe_return_a_400_when_data_is_missing() {
 }
 
 static TRACING: Lazy<()> = Lazy::new(|| {
-    let subscriber = get_subscriber("test".into(), "debug".into());
-    init_subscriber(subscriber);
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
 });
 pub struct TestApp {
     pub address: String,
