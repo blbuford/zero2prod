93f12c15d5706b6af98c2f7247743c921e622b1d
Brett Buford <blbuford@gmail.com>
2022-04-11 23:16:21 -0700

    delivery task queue and associated background worker

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::fmt::{Debug, Display};
+use tokio::task::JoinError;
 use zero2prod::startup::Application;
 use zero2prod::{configuration::get_configuration, telemetry::*};
+use zero2prod::issue_delivery_worker::run_worker_until_stopped;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -7,7 +10,44 @@ async fn main() -> anyhow::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let application = Application::build(configuration).await?;
-    application.run_until_stopped().await?;
+    let application = tokio::spawn(
+        Application::build(configuration.clone())
+        .await?
+        .run_until_stopped());
+
+    let worker = tokio::spawn(run_worker_until_stopped(configuration));
+
+    tokio::select! {
+        o = application => report_exit("API", o),
+        o = worker => report_exit("Background Worker", o),
+    };
+
     Ok(())
+}
+
+fn report_exit(
+    task_name: &str,
+    outcome: Result<Result<(), impl Debug + Display>, JoinError>
+) {
+    match outcome {
+        Ok(Ok(())) => {
+            tracing::info!("{} has exited", task_name)
+        }
+        Ok(Err(e)) => {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "{} failed",
+                task_name
+            )
+        },
+        Err(e) =>{
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "{} failed to complete",
+                task_name
+            )
+        }
+    }
 }
--- tests/api/newsletters.rs
+++ tests/api/newsletters.rs
@@ -1,8 +1,11 @@
 use crate::helpers::{assert_is_redirect_to, spawn_app, ConfirmationLinks, TestApp};
 use std::time::Duration;
+use fake::Fake;
+use fake::faker::internet::en::SafeEmail;
+use fake::faker::name::en::Name;
 use uuid::Uuid;
 use wiremock::matchers::{any, method, path};
-use wiremock::{Mock, ResponseTemplate};
+use wiremock::{Mock, MockBuilder, ResponseTemplate};
 
 #[tokio::test]
 async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
@@ -125,8 +128,7 @@ async fn newsletter_creation_is_idempotent() {
     create_confirmed_subscriber(&app).await;
     app.do_login().await;
 
-    Mock::given(path("/email"))
-        .and(method("POST"))
+    when_sending_an_email()
         .respond_with(ResponseTemplate::new(200))
         .expect(1)
         .mount(&app.email_server)
@@ -187,8 +189,55 @@ async fn concurrent_form_submission_is_handled_gracefully() {
     );
 }
 
+#[tokio::test]
+async fn transient_errors_do_not_cause_duplicate_deliveries_on_retries() {
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+    create_confirmed_subscriber(&app).await;
+    app.do_login().await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter Title",
+        "text": "Newsletter body as plain text",
+        "html": "<p>Newsletter body as HTML</p>",
+        "idempotency_key": Uuid::new_v4().to_string(),
+    });
+
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(200))
+        .up_to_n_times(1)
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(500))
+        .up_to_n_times(1)
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let response = app.post_newsletters(&newsletter_request_body).await;
+    assert_eq!(response.status().as_u16(), 500);
+
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(500))
+        .expect(1)
+        .named("Delivery retry")
+        .mount(&app.email_server)
+        .await;
+
+    let response = app.post_newsletters(&newsletter_request_body).await;
+    assert_eq!(response.status().as_u16(), 303);
+}
+
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.local";
+    let name: String = Name().fake();
+    let email: String = SafeEmail().fake();
+    let body = serde_urlencoded::to_string(&serde_json::json!({
+        "name": name,
+        "email": email
+    }))
+        .unwrap();
 
     let _mock_guard = Mock::given(path("/email"))
         .and(method("POST"))
@@ -219,4 +268,9 @@ async fn create_confirmed_subscriber(app: &TestApp) {
         .unwrap()
         .error_for_status()
         .unwrap();
+}
+
+fn when_sending_an_email() -> MockBuilder {
+    Mock::given(path("/email"))
+        .and(method("POST"))
 }
